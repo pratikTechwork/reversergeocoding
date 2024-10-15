6292af99e3d4f20699f29705e385d7a2c3e9cd5d
pratikTechwork <127339703+pratikTechwork@users.noreply.github.com>
2024-10-15 16:58:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,29 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-    user: "postgres",
+    user: "",
     host: '',
-    database: "postgres",
+    database: "",
     password: '',
-    port: 5432,
+    port: ,
 });
-let scanlogs = []
-async function querydb(){
-    let data = await pool.query(`select id, qr_id,scan_1_location from scan_log`);
-    scanlogs = data.rows
-    getcity()
-}
-querydb()
-
-function getcity(){
-    for (let index = 0; index < scanlogs.length; index++) {
-       setTimeout(()=>{
-        let lat = scanlogs[index].scan_1_location[0]
-        let lon = scanlogs[index].scan_1_location[1]
-        fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${lon}&apiKey=123123`)
-          .then(response => response.json())
-          .then(async result =>{
-            console.log(result.features[0].properties.state)
-            let statename = result.features[0].properties.state
-            await pool.query(`update scan_log set state='${statename}' where id=${scanlogs[index].id}`);
-          })
-          .catch(error => console.log('error', error));
-       },1000 * index)
+const key = ''
+const getquery = 'select id , scan_1_location , state from scan_log where state is null'
+const updatequery = `update scan_log set state = $1 , local_address = $2 where id=$3`
+async function getscan_without_state(){
+   try {
+    const result = await pool.query(getquery)
+    let scans = result.rows;
+    for (let index = 0; index < scans.length; index++) {
+        setTimeout(async() => {
+        const {id ,scan_1_location:[latitude,longitude]} = scans[index]
+        const link = await fetch(`https://geocode.maps.co/reverse?lat=${latitude}&lon=${longitude}&api_key=${key}`)
+        const {display_name , address:{state}} = await link.json()
+        const updatecolumns = await pool.query(updatequery,[state,display_name,id])
+        console.log(updatecolumns.command);
+            }, index * 1200);
     }
+   } catch (error) {
+    console.log(error);
+   }
 }
+getscan_without_state()
